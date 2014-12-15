ef98da21a05fae2ba00984e2495e5c353f6a9c8d
JD Isaacks <jd@jisaacks.com>
2014-12-14 19:32:12 -0500

    Add Datastore via nedb

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-var express = require("express");
-var mincer  = require("mincer");
+var express     = require("express");
+var mincer      = require("mincer");
+var Datastore   = require('nedb');
+var bodyParser  = require('body-parser');
 
 var app = express();
 var env = new mincer.Environment();
+var db  = new Datastore("records");
 
 env.appendPath("assets/javascripts");
 env.appendPath("assets/stylesheets");
 env.appendPath("assets/images");
 
 app.use("/assets", mincer.createServer(env));
+app.use(bodyParser.json());
 app.set('view engine', 'jade');
 
 app.get("/", function(req, res) {
   res.render("index");
 });
 
-app.listen(8025);
+var restful = require("./restdb");
+restful(app, db, "/things");
+
+db.loadDatabase(function (err) {
+  if (err) throw err;
+  app.listen(8025);
+});
