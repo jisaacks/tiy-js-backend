4669efdf55eb6c62e87fbb35af6bf0deccfff19a
JD Isaacks <jd@jisaacks.com>
2014-12-20 22:55:45 -0500

    Use json_api for rest endpoints

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var express     = require("express");
 var mincer      = require("mincer");
 var Datastore   = require('nedb');
 var bodyParser  = require('body-parser');
+var json_api    = require('json_api');
 
 var app = express();
 var env = new mincer.Environment();
-var db  = new Datastore("records");
+var db  = new Datastore("records.nedb");
 
 env.appendPath("assets/javascripts");
 env.appendPath("assets/stylesheets");
@@ -13,14 +14,13 @@ env.appendPath("assets/images");
 
 app.use("/assets", mincer.createServer(env));
 app.use(bodyParser.json());
+app.use("/records", json_api(db));
+
 app.set('view engine', 'jade');
 
 app.get("/", function(req, res) {
   res.render("index");
 });
-
-var restful = require("./restdb");
-restful(app, db, "/things");
 
 db.loadDatabase(function (err) {
   if (err) throw err;
