d35e8cef802e8b70d85a66b8a3aa283c230bf0e3
JD Isaacks <jd@jisaacks.com>
2015-01-07 09:54:58 -0500

    Make gulp minify js and css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp    = require('gulp');
 var Mincer  = require('mincer');
 var mince   = require("gulp-mincer");
 var rename  = require("gulp-rename");
+var uglify  = require("gulp-uglify");
+var minify  = require("gulp-minify-css");
 var env     = require("./mincer-env");
 
 var nodir = function(path) {
@@ -9,8 +11,15 @@ var nodir = function(path) {
 };
 
 gulp.task('default', function() {
-  gulp.src("assets/**/application.*")
+  gulp.src("assets/**/application.js")
     .pipe( mince(env) )
     .pipe( rename(nodir) )
+    .pipe( uglify() )
+    .pipe( gulp.dest("public/assets") );
+
+  gulp.src("assets/**/application.css")
+    .pipe( mince(env) )
+    .pipe( rename(nodir) )
+    .pipe( minify() )
     .pipe( gulp.dest("public/assets") );
 });
